92e58076ce8bf507980bd6daa0f5a04ca9dfc9da
Stephen McDonald <stephenmcdonald8787@gmail.com>
2020-04-30 17:23:56 -0400

    seeded DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Song.create(title: "I Really Like You", length: 209, play_count: 760847)
+Song.create(title: "Call Me Maybe", length: 199, play_count: 65862)
+Song.create(title: "Run Away With Me", length: 253, play_count: 521771)
+Song.create(title: "Party For One", length: 269, play_count: 623547)
